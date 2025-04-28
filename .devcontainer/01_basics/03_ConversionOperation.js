bb449245287f1c8337ea064ef3074ffdbc9fbce4
Tapaswini Dash <dashtapaswini07@gmail.com>
2025-04-28 20:26:16 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score = 33
+
+console.log(typeof score);
+console.log (typeof(score));
+
+
+let score1 = "33abc"
+
+console.log(typeof score1);
+console.log (typeof(score1));
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber); // NaN ==> not a no.
+//let valueInNumber = String(score1)
+//console.log(typeof valueInNumber);
+console.log(valueInNumber);
+  // "33" ==> 33
+  //"33abc" ==> NaN
+  //true==> 1; false ==>0 
+
+  let isloggedin = 1
+
+  let booleanIsLoggedIn = Boolean(isloggedin)
+  console.log(booleanIsLoggedIn);
+  // 1==> true; 0==> false
+  //"" ==> false
+  // "sneha"==> true
+
+  let score2= 12
+  let valueInNumber1 = String(score2)
+  console.log(typeof valueInNumber1);
+  console.log(valueInNumber1);
