89f7e771fe75768709f897aebf3b793fd963d43a
Tapaswini Dash <dashtapaswini07@gmail.com>
2025-04-28 19:44:18 +0000

    datatypes

--- .devcontainer/01_basics/01_varriables.js
+++ .devcontainer/01_basics/01_varriables.js
@@ -1,5 +1,17 @@
 const accountID = 123456
 let accountEmail="dashtapaswini@gmail.com"
 var accountPassword = "12345"
-
+//accountID = 2 //TypeError: Assignment to constant variable.
+accountEmail = "hello@.com"
+//accountState;ReferenceError: accountState is not defined
+let accountState1;
+accountPassword = "09876"
+accountCity = "Puri"
 console.log(accountID);
+console.log(accountPassword);
+console.log(accountEmail);
+console.table([accountID, accountEmail,accountPassword,accountCity, accountState1]);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3) // always try to build code readability for ensuring professional experience
+console.log ("Sneha")
+let name = "Sneha"
+let age = 18
+let isloggedin = true
+
+//number ==> 2 to the power 53
+// bigint
+//string ==> ""
+//boolean ==> true/false ( not True/False)
+//null ==> standalaone value // intentionally also i can add let state = null
+//undefined ==> value has been not defined
+//symbol ==>unique
+
+
+//object
+
+console.log (typeof "Sneha") // string
+console.log (typeof 18) //number
+console.log (typeof true) // boolean 
+console.log (typeof null)// object
+console.log(typeof undefined)//undefined
